82a367a93074cf6af033cdca97b951a88ef58f1e
David Haslem <therabidbanana@gmail.com>
2012-03-30 18:22:26 -0400

    Fix test

--- spec/dtime/connection/response/helpers_spec.rb
+++ spec/dtime/connection/response/helpers_spec.rb
@@ -4,7 +4,7 @@ describe Dtime::Connection::Response::Helpers do
 
   let(:dtime) { Dtime.new }
   # FIXME - Old reference
-  let(:res)    { dtime.home.result }
+  let(:res)    { dtime.home.response }
 
   before do
     stub_get("/").
