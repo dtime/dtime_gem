b15b261dae12fb4ebebbb5345b54453d672afbc2
David Haslem <therabidbanana@gmail.com>
2012-04-09 17:28:49 -0400

    Try dtime: prefixed rels if unprefixed not found

--- lib/dtime/resources/resource_factory.rb
+++ lib/dtime/resources/resource_factory.rb
@@ -14,10 +14,14 @@ module Dtime
 
 
       # Find the link for a given rel from the client's last request
+      # Will first try raw rel, then fall back to curie uri prefixed with dtime
+      #
+      # If rel not found, raises Dtime::UnknownRel
       # Opts, if provided, will be used for a uri template
       def turn_rel_into_link(rel, opts = {})
         return rel if rel.respond_to?(:rel)
         ret = client.link_for_rel(rel, opts.merge(:force => true))
+        ret = client.link_for_rel("dtime:#{rel}", opts) unless ret
         raise Dtime::UnknownRel.new("Rel '#{rel}' not recognized", {}) unless ret
         ret
       end
