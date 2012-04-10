7d08845af8794d0d23e0280f5037b92cb9eac5c9
David Haslem <therabidbanana@gmail.com>
2012-04-10 14:09:49 -0400

    Fix

--- lib/dtime/resources/resource.rb
+++ lib/dtime/resources/resource.rb
@@ -29,6 +29,11 @@ module Dtime
 
       def result
         @response.result if @response
+        if @reponse && @response.respond_to?(:result)
+          @response.result.is_a?(Array)  ? @response.result : @response
+        else
+          @response
+        end
       end
 
       def get_result
